7dca7187f693b712a81250d9d0126460f81cce7a
Md-Faiz-Alam <mdfaiz3388@gmail.com>
2026-05-03 15:23:35 +0530

    update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,23 +1,25 @@
-const BASE_URL = "http://127.0.0.1:5000/api"
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 export const api = {
   async get(endpoint) {
-    const res = await fetch(`${BASE_URL}${endpoint}`)
-    return res.json()
+    const res = await fetch(`${BASE_URL}${endpoint}`);
+    return res.json();
   },
+
   async post(endpoint, data) {
     const res = await fetch(`${BASE_URL}${endpoint}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data)
-    })
-    return res.json()
-  }
-}
+      body: JSON.stringify(data),
+    });
+
+    return res.json();
+  },
+};
 
 export const auth = {
   getUser: () => JSON.parse(localStorage.getItem("user")),
   setUser: (user) => localStorage.setItem("user", JSON.stringify(user)),
   logout: () => localStorage.removeItem("user"),
-  isLoggedIn: () => !!localStorage.getItem("user")
-}
+  isLoggedIn: () => !!localStorage.getItem("user"),
+};
